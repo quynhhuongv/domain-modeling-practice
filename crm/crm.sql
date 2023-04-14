1626588b8bdeda2a1c130ba64dc8e14eeeafe772
Huong Vu <huong.vu@kellogg.northwestern.edu>
2023-04-14 20:48:09 +0000

    commit pls

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,45 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salespeople;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS industries;
 -- CREATE TABLES
+
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT
+);
+
+CREATE TABLE salespeople (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    notes TEXT,
+    created_at TEXT,
+    salesperson_id INTEGER,
+    contact_id INTEGER
+);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT
+);
+
+
